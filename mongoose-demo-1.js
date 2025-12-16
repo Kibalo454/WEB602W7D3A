b9541373a485cf0ef11dee6ce108db2ecbdc7006
Kibalo454 <k.passigue.9247@westcliff.edu>
2025-12-15 19:07:47 -0500

    class completed

--- mongoose-demo-1.js
+++ mongoose-demo-1.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true);
-mongoose.connect('mongodb://localhost:27017/test'); //OR
-//mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
+const fruitSchema = new mongoose.Schema({
+  name: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const apple = new Fruit({
+  name: "Apple"
+});
+
+apple.save().then(() => {
+  console.log("Apple saved");
+  mongoose.connection.close();
 });
--- mongoose-demo-2.js
+++ mongoose-demo-2.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
-mongoose.connect('mongodb://localhost:27017/test'); //OR
-//mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
+const catSchema = new mongoose.Schema({
+  name: String
 });
 
+// schema method
+catSchema.methods.speak = function () {
+  return `Meow name is ${this.name}`;
+};
 
+const Cat = mongoose.model("Cat", catSchema);
+
+const kitty = new Cat({ name: "fluffy" });
+
+console.log(kitty.speak());
+
+mongoose.connection.close();
